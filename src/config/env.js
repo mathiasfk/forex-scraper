037a8ff95028b584a673f4be8d192e802a043a10
Mathias Kriebel <mathias.kriebel@gmail.com>
2024-10-29 09:21:12 -0300

    feat: run in loop

--- src/config/env.js
+++ src/config/env.js
@@ -2,4 +2,5 @@ import 'dotenv/config';
 
 export const config = {
   apiKey: process.env.API_KEY,
+  updateFrequencyInMinutes: process.env.UPDATE_FREQUENCY_MINUTES || 10
 };
--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
 import { fetchAverage } from './services/polygonApiHistoryService.js';
 import { fetchCurrentRate } from './services/tradingViewCurrentService.js';
 import { showInfoDialog } from './utils/dialogUtils.js';
+import { wait } from './utils/timeUtils.js';
+import { config } from './config/env.js';
 
 async function main() {
   try {
-    const avg = await fetchAverage();
-    const currentRate = await fetchCurrentRate();
+    while(true){
+      const avg = await fetchAverage();
+      const currentRate = await fetchCurrentRate();
 
-    console.log(`Cotação Atual: ${currentRate}`);
-    console.log(`Média da Semana: ${avg}`);
+      console.log(`Cotação Atual: ${currentRate}`);
+      console.log(`Média da Semana: ${avg}`);
 
-    if (currentRate < avg) {
-      showInfoDialog(`
-        Considere comprar dólares agora! \n
-        Cotação atual: ${currentRate.toLocaleString("pt-br")} \n
-        Média da última semana: ${avg.toLocaleString("pt-br")}
-      `);
+      if (currentRate < avg) {
+        showInfoDialog(`
+          Considere comprar dólares agora! \n
+          Cotação atual: ${currentRate.toLocaleString("pt-br")} \n
+          Média da última semana: ${avg.toLocaleString("pt-br")}
+        `);
+      }
+      await wait(config.updateFrequencyInMinutes * 60);
     }
   } catch (error) {
     console.error(`Erro: ${error.message}`);
